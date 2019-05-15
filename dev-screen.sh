93181cec6dedea6e97052150db16ec71877d51d8
Maru Newby <marun@redhat.com>
2019-05-15 11:31:56 -0700

    Add terminals to dev-screen config

--- dev-screen.sh
+++ dev-screen.sh
@@ -4,3 +4,6 @@ screen_cmd="screen -S maru-dev -fn"
 
 $screen_cmd -dm -t main-edit bash
 $screen_cmd -X screen -t main-cli bash
+$screen_cmd -X screen -t obs-1 bash
+$screen_cmd -X screen -t obs-2 bash
+screen -d -R maru-dev
