d4b80fdada1de5ab558ec2dbac59dc80c05db26f
Maru Newby <marun@redhat.com>
2019-08-05 19:15:30 +0000

    Update fedup script

--- fedup.sh
+++ fedup.sh
@@ -7,10 +7,6 @@ set -o pipefail
 # Delete clusters (delete-clusters.sh script kills my machine)
 kind get clusters | xargs -I {} kind delete cluster --name {}
 
-## Deploy with image in local insecure registry
-#CONTAINER_REGISTRY_HOST=172.17.0.3:5000 OVERWRITE_KUBECONFIG=y ./scripts/create-clusters.sh
-#./scripts/deploy-federation.sh 172.17.0.3:5000/federation-v2:test cluster2
-
 # Deploy with image in docker hub
-KIND_TAG="v1.14.0" OVERWRITE_KUBECONFIG=y ./scripts/create-clusters.sh
-./scripts/deploy-federation.sh docker.io/maru/kubefed:test cluster2
+KIND_TAG=v1.14.2 CONFIGURE_INSECURE_REGISTRY_CLUSTER= OVERWRITE_KUBECONFIG=y ./scripts/create-clusters.sh
+./scripts/deploy-kubefed.sh docker.io/maru/kubefed:test cluster2
