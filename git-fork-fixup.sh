4bcdff5c24a3f9017baca87fc7f7ed2388baf75c
Maru Newby <me@maru.ca>
2021-01-28 11:25:03 -0800

    Update fork fixup helper to user marun/origin instead of origin/upstream

--- git-fork-fixup.sh
+++ git-fork-fixup.sh
@@ -2,6 +2,11 @@
 
 repo_name=$(basename $(pwd))
 
-git remote rename origin upstream
-git remote add origin https://github.com/marun/${repo_name}
-git remote set-url --push origin git@github.com:marun/${repo_name}.git
+# Fix repos previously configured to name the personal fork origin
+if git remote | grep -q upstream; then
+  git remote rename origin marun
+  git remote rename upstream origin
+else
+  git remote add marun https://github.com/marun/${repo_name}
+  git remote set-url --push marun git@github.com:marun/${repo_name}.git
+fi
