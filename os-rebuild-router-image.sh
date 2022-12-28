15b4cdf15f36e67ca9cf9630199ca80c415dd825
Maru Newby <me@maru.ca>
2022-12-27 21:39:01 -0800

    Remove stale scripts

--- os-rebuild-router-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-OS_RELEASE=n hack/build-go.sh
-cp _output/local/bin/linux/amd64/openshift images/origin/bin/
-docker build -t openshift/origin images/origin
-docker build -t openshift/origin-haproxy-router images/router/haproxy/
--- vos-terminate.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Terminate the aws instance used to run tests with vagrant-openshift
-
-vagrant modify-instance -r marun-dev_terminate -s && rm -rf .vagrant
